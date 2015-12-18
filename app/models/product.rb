47f18e6aae309f7b701e42c79f2a70dd1f4d672a
doskan216 <you@example.com>
2015-12-18 04:54:25 +0000

    トップページにカートを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,19 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :name, :company, :date, :description, :large_category, :small_category,
             :remaining_count, :price, :image_url, presence: true
+
+  private
+  #この商品を参照している品目がないか確認する
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base,'品目が存在します')
+      return false
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   resources :detail_pages
   get 'store/index'
 
